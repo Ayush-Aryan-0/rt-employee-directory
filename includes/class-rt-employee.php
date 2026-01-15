11f779e5e4ecf7cd027c6207c6b4c984a4dde7c4
Ayush Aryan <ayusharyan00000@gmail.com>
2026-01-15 22:13:03 +0530

    UI: Add meta boxes for Employee Designation and Email

--- includes/class-rt-employee.php
+++ includes/class-rt-employee.php
@@ -34,6 +34,44 @@ class Rt_Employee_Directory {
 
         register_post_type( 'rt_employee', $args );
     }
+    /**
+     * Add Meta Boxes for Designation and Email
+     */
+    public function add_meta_boxes() {
+        add_meta_box(
+            'rt_employee_details',      // ID
+            'Employee Details',         // Title
+            array( $this, 'render_meta_box' ), // Callback
+            'rt_employee',              // Screen (CPT Name)
+            'normal',                   // Context
+            'high'                      // Priority
+        );
+    }
+
+    /**
+     * Render the HTML for Meta Boxes
+     * @param object $post The post object
+     */
+    public function render_meta_box( $post ) {
+        // SECURITY: Create a Nonce field
+        wp_nonce_field( 'rt_save_employee_data', 'rt_employee_nonce' );
+
+        // Get existing values
+        $designation = get_post_meta( $post->ID, '_rt_designation', true );
+        $email = get_post_meta( $post->ID, '_rt_email', true );
+
+        // HTML Output
+        ?>
+        <p>
+            <label for="rt_designation">Designation:</label><br>
+            <input type="text" id="rt_designation" name="rt_designation" value="<?php echo esc_attr( $designation ); ?>" class="widefat">
+        </p>
+        <p>
+            <label for="rt_email">Email:</label><br>
+            <input type="email" id="rt_email" name="rt_email" value="<?php echo esc_attr( $email ); ?>" class="widefat">
+        </p>
+        <?php
+    }
     
 }
 
